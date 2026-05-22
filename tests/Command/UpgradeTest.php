24749bfaf27f8c68051b54b3a9f2959b81c18578
Jasper Tey <jasper.tey@gmail.com>
2026-05-22 07:53:26 -0400

    chore: remove legacy 0.x upgrade test and dataset

--- tests/Command/UpgradeTest.php
+++ tests/Command/UpgradeTest.php
@@ -1,34 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
-
-it('can upgrade 0.x config to 1.x', function (string $pathToOldConfig, array $expectedValues) {
-    $configFilePath = config_path('ddd.php');
-
-    File::copy($pathToOldConfig, $configFilePath);
-
-    expect(file_exists($configFilePath))->toBeTrue();
-
-    $this->artisan('ddd:upgrade')
-        ->expectsOutputToContain('Configuration upgraded successfully.')
-        ->execute();
-
-    Artisan::call('config:clear');
-
-    $expectedValues = Arr::dot($expectedValues);
-
-    $configAsArray = require config_path('ddd.php');
-
-    foreach ($expectedValues as $key => $value) {
-        expect(data_get($configAsArray, $key))
-            ->toEqual($value, "Config {$key} does not match expected value.");
-    }
-
-    // Delete the config file after the test
-    unlink($configFilePath);
-})->with('configUpgrades');
 
 it('skips upgrade if config file was not published', function () {
     $path = config_path('ddd.php');
